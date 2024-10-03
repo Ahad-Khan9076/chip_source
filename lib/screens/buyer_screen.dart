df0a28a3f2e8c4829e8060b5e47336fdb6312135
Ahad Ali khan <ahadalikhan1525@gmail.com>
2024-10-03 12:20:42 +0500

    Create buyer_screen.dart

---
+++ lib/screens/buyer_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BuyerScreen extends StatefulWidget {
+  const BuyerScreen({super.key});
+
+  @override
+  State<BuyerScreen> createState() => _BuyerScreenState();
+}
+
+class _BuyerScreenState extends State<BuyerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
